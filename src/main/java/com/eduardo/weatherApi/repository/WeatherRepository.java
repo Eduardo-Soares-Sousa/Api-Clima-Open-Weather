2c54e90b1619b04e6eb949844bb2261d1943b2d0
Eduardo Soares <eduardo_soares001@outlook.com>
2026-02-28 16:17:45 -0300

    adding weather history repository

--- src/main/java/com/eduardo/weatherApi/repository/WeatherRepository.java
+++ src/main/java/com/eduardo/weatherApi/repository/WeatherRepository.java
@@ -1,4 +1,12 @@
 package com.eduardo.weatherApi.repository;
 
-public interface WeatherRepository {
+import com.eduardo.weatherApi.model.entity.WeatherHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WeatherRepository extends JpaRepository<WeatherHistory, Long> {
+    List<WeatherHistory> findByCity(String city);
 }
