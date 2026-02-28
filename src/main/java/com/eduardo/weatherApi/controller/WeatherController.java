7f47a7f73e0ebfbe91975d59bfa9320684fb6f4c
Eduardo Soares <eduardo_soares001@outlook.com>
2026-02-28 18:00:57 -0300

    implementing WeatherController

--- src/main/java/com/eduardo/weatherApi/controller/WeatherController.java
+++ src/main/java/com/eduardo/weatherApi/controller/WeatherController.java
@@ -1,4 +1,35 @@
 package com.eduardo.weatherApi.controller;
 
+import com.eduardo.weatherApi.business.service.WeatherService;
+import com.eduardo.weatherApi.model.dto.WeatherResponse;
+import com.eduardo.weatherApi.model.entity.WeatherHistory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/weather")
 public class WeatherController {
+    private WeatherService weatherService;
+
+    @GetMapping("/{city}")
+    public ResponseEntity<WeatherResponse> getWeather(@RequestParam String city) {
+        return ResponseEntity.ok(weatherService.showingWeather(city));
+    }
+
+    @GetMapping("/history/{city}")
+    public ResponseEntity<List<WeatherHistory>> getWeatherHistoryByCity(@RequestParam String city) {
+        return ResponseEntity.ok(weatherService.getCityWeatherHistory(city));
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<WeatherHistory>> getAllWeatherHistory() {
+        return ResponseEntity.ok(weatherService.getWeatherHistory());
+    }
 }
